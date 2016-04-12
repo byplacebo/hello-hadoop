ad68bc138c0f62ef78e0c93a9f10ee2690dc575a
byplacebo <placebo2me@gmail.com>
2016-04-12 17:14:37 +0900

    write wordcount sample

---
+++ wordcount/src/main/java/me/placebo/wordcount/WordCountMapper.java
@@ -0,0 +1,28 @@
+package me.placebo.wordcount;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+/**
+ * @author 임형태
+ * @since 2016.04.12
+ */
+public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    @Override
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        StringTokenizer itr = new StringTokenizer(value.toString());
+        while (itr.hasMoreTokens()) {
+            word.set(itr.nextToken());
+            context.write(word, one);
+        }
+    }
+}
